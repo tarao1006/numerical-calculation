85b30c046d6b92c31793dabcda277a1b9768f06b
tarao1006 <taiga4562@gmail.com>
2020-07-07 01:40:21 +0900

    Add frontend

---
+++ frontend/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+
+const App = () => {
+
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+ReactDom.render(
+  <App />, document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "frontend/src/index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules:[
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env","@babel/preset-react"]
+          }
+        },
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: path.resolve(__dirname, "frontend/src/index.html")
+      }
+    )
+  ]
+}
