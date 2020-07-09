fa1b97838ace2934a65d622f1af8c746510c37a9
tarao1006 <taiga4562@gmail.com>
2020-07-10 00:33:49 +0900

    Add execute function

--- frontend/src/calculator/linear_equation/jacobi_method.jsx
+++ frontend/src/calculator/linear_equation/jacobi_method.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import MatrixContainer from '../../components/matrixcontainer'
 import Vector from '../../components/matrix/vector'
 import useJacobiMethodLinearEquation from '../../actions/jacobiMethodLinearEquationAction'
@@ -8,15 +9,30 @@ const JacobMethodLinearEquation = () => {
   const [ status, setStatus ] = useState(false)
   const [ executed, setExecuted ] = useState(false)
   const [ solutionVector, setSolutionVector ] = useState([])
+  const { size, coefficientMatrix, rightHandSideVector } = useJacobiMethodLinearEquation()
 
-  const dummyExecute = () => {
-    console.log('executed')
+  const execute = async () => {
+    setExecuted(false)
+    try {
+      const result = await axios.post(
+        'http://localhost:3001/api/v1/linear_equation/jacobi_method', {
+          "size": size,
+          "matrix": coefficientMatrix,
+          "b": rightHandSideVector
+        })
+
+        setStatus(result.data.status === 'SUCCESS')
+        setSolutionVector(result.data.ans)
+        setExecuted(true)
+      } catch (error) {
+        setStatus(false)
+    }
   }
 
   return (
     <MatrixContainer
       title={ title }
-      execute={ dummyExecute }
+      execute={ execute }
       status={ status }
       executed={ executed }
       result={
