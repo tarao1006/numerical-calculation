634122d95ff4928bbc6d324c18187a40a8444c73
tarao1006 <taiga4562@gmail.com>
2020-07-09 22:44:08 +0900

    Add useJacobiMethodLinearEquation

--- frontend/src/calculator/linear_equation/jacobi_method.jsx
+++ frontend/src/calculator/linear_equation/jacobi_method.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import useJacobiMethodLinearEquation from '../../actions/jacobiMethodLinearEquationAction'
 
 const JacobMethodLinearEquation = () => {
+  const [ size, coefficientMatrix, rightHandSideVector, update,
+    increment, decrement, changeCoefficientMatrixValue, changeRightHandSideVectorValue ] = useJacobiMethodLinearEquation()
 
-  return (
+    return (
     <>
       ヤコビ法
     </>
