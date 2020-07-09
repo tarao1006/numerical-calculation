78dda8480748a468b74d59990a14a5b63b5d7dde
tarao1006 <taiga4562@gmail.com>
2020-07-10 00:56:55 +0900

    Change hover decoration

--- frontend/src/components/executebutton.jsx
+++ frontend/src/components/executebutton.jsx
@@ -12,21 +12,21 @@ export const ExecuteButton = ({ execute }) => {
 
 export const Button = styled.button`
   width: 200px;
-  color: black;
+  color: white;
   font-size: 1em;
   margin: 1em;
-  padding: 0.25em 1em;
+  padding: 0.8em 1em;
   border: none;
   border-radius: 3px;
   box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.1), 0px 2px 10px 0px rgba(0, 0, 0, 0.08);
   transition: all 0.25s ease 0s;
-  background: white;
+  background: #0073EC;
   cursor: pointer;
   outline: none;
   appearance: none;
 
    &:hover {
-    transform: translateY(-3px);
+    background: #0162CC;
   }
 `
 
