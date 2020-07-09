ecbdaedcc945224e3f8d829480545dad24ac233c
tarao1006 <taiga4562@gmail.com>
2020-07-10 00:33:27 +0900

    Enable to use async await

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,11 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env","@babel/preset-react"]
+            presets: [[
+              "@babel/preset-env", {
+                useBuiltIns: "usage",
+                corejs: 3
+              }],"@babel/preset-react"]
           }
         },
         exclude: /node_modules/,
