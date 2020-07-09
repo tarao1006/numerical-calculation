02dc7c2f8c58ba71dd70b966285604e84f4acf62
tarao1006 <taiga4562@gmail.com>
2020-07-09 23:42:58 +0900

    Add some variables to main container

--- frontend/src/calculator/linear_equation/jacobi_method.jsx
+++ frontend/src/calculator/linear_equation/jacobi_method.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import MatrixContainer from '../../components/matrixcontainer'
+import Vector from '../../components/matrix/vector'
 import useJacobiMethodLinearEquation from '../../actions/jacobiMethodLinearEquationAction'
 
 const JacobMethodLinearEquation = () => {
-  const [ size, coefficientMatrix, rightHandSideVector, update,
-    increment, decrement, changeCoefficientMatrixValue, changeRightHandSideVectorValue ] = useJacobiMethodLinearEquation()
+  const title = "ヤコビ法"
+  const [ status, setStatus ] = useState(false)
+  const [ executed, setExecuted ] = useState(false)
+  const [ solutionVector, setSolutionVector ] = useState([])
 
-    return (
-    <>
-      ヤコビ法
-    </>
+  const dummyExecute = () => {
+    console.log('executed')
+  }
+
+  return (
+    <MatrixContainer
+      title={ title }
+      execute={ dummyExecute }
+      status={ status }
+      executed={ executed }
+      result={
+        executed &&
+          <Vector
+            size={ solutionVector.length }
+            values={ solutionVector }
+            readOnly={ true }
+          />
+      }
+      setStatus={ setStatus }
+      setExecuted={ setExecuted }
+    / >
   )
 }
 
