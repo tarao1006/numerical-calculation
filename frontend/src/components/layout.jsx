e82f4fe20db21ba8c43e10aa3bd31c647f16cf29
tarao1006 <taiga4562@gmail.com>
2020-07-10 10:23:38 +0900

    Add grid layout

--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import styled from 'styled-components'
+import SideNav from './sidenav'
 
 const Layout = ({ children }) => {
 
   return (
-    <Main>
-      { children }
-    </Main>
+    <Container>
+      <SideGrid>
+        <SideNav />
+      </SideGrid>
+      <MainGrid>
+        { children }
+      </MainGrid>
+    </Container>
   )
 }
 
 export default Layout
 
-const Main = styled.div`
+const MainGrid = styled.div`
   margin: 0 auto;
   width: 800px;
+  grid-column: 2;
+`
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 220px 1fr;
+  justify-content: center;
+`
+
+const SideGrid = styled.div`
+  grid-column: 1;
 `
