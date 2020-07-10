763d77a2e2032dfafda46de829454260378428c8
tarao1006 <taiga4562@gmail.com>
2020-07-10 23:50:53 +0900

    Add lower and upper triangular matrix

---
+++ frontend/src/components/matrix/lower_triangular_matrix.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components'
+import MatrixCell from './matrixcell'
+
+const LowerTriangularMatrix = ({ rowCount, columnCount, values, id }) => {
+
+  let mat = []
+  for (let i = 0; i < rowCount; ++i) {
+    let row = []
+    for (let j = 0; j < columnCount; ++j) {
+      if ( i <= j) {
+        row.push(
+          <td key={ `${id}${(i * rowCount + j).toString()}` }>
+            <MatrixCell
+              x={ i }
+              y={ j }
+              val={ values[i][j] }
+            />
+          </td>)
+      }
+    }
+    mat.push(<tr key={ i }>{ row }</tr>)
+  }
+
+  return (
+    <Table>
+      <tbody>
+        { mat }
+      </tbody>
+    </Table>
+  )
+}
+
+export default LowerTriangularMatrix
+
+const Table = styled.table`
+  border-collapse: collapse;
+  margin: 0 auto;
+  box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.1), 0px 0px 0px 0px rgba(0, 0, 0, 0.08);
+`
