7d3122f0fba88f5fb9953edd541e8a39f271aeb7
tarao1006 <taiga4562@gmail.com>
2020-07-10 13:40:17 +0900

    Remove import

--- frontend/src/components/title.jsx
+++ frontend/src/components/title.jsx
@@ -1,19 +1,1 @@
-import React from 'react'
-import styled from 'styled-components'
-
 export const siteTitle = "数値計算ライブラリ"
-
-const Title = ({ title }) => {
-
-  return (
-    <H1>
-      { title }
-    </H1>
-  )
-}
-
-export default Title
-
-const H1 = styled.h1`
-  text-align: center;
-`
