7d4f81eeb6c141436eaea2c121ba94d5227bc037
tarao1006 <taiga4562@gmail.com>
2020-07-11 08:44:51 +0900

    Add reducers for forward and backward substitution

---
+++ frontend/src/actions/other/backward_substitution.js
@@ -0,0 +1,8 @@
+import useMatrix from '../matrix'
+
+const useBackwardSubstitution = () => {
+
+  return useMatrix({kind: 'backwardSubstitution'})
+}
+
+export default useBackwardSubstitution
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,15 @@ import { applyMiddleware, combineReducers, createStore } from 'redux'
 import jacobiMethodLinearEquation from './reducers/jacobiMethodLinearEquationReducer'
 import gaussSeidelMethodLinearEquation from './reducers/gaussSeidelMethodLinearEquationReducer'
 import sorMethodLinearEquation from "./reducers/sorMethodLinearEquationReducer"
+import forwardSubstitution from './reducers/forwardSubstitutionReducer'
+import backwardSubstitution from './reducers/backwardSubstitutionReducer'
 
 const store = createStore(combineReducers({
   jacobiMethodLinearEquation,
   sorMethodLinearEquation,
-  gaussSeidelMethodLinearEquation
+  gaussSeidelMethodLinearEquation,
+  forwardSubstitution,
+  backwardSubstitution
 }), applyMiddleware())
 
 export default store
