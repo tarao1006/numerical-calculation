d8a019ab82c0d89263a7462ae2d59627738fb99a
tarao1006 <taiga4562@gmail.com>
2020-07-10 21:07:13 +0900

    Add change matrix and vector value

--- frontend/src/__tests__/matrix.test.js
+++ frontend/src/__tests__/matrix.test.js
@@ -1,4 +1,10 @@
-import { updateValues, validateSize, changeSize, changeMatrixValue, changeVectorValue} from '../lib/linearequation'
+import {
+  updateValues,
+  validateSize,
+  changeSize,
+  changeMatrixValue,
+  changeVectorValue
+} from '../lib/linearequation'
 
 const checkTwoDimMatrixSize = mat => {
   let res = true
@@ -39,4 +45,39 @@ test('changeSize must return designated size matrix and vector', () => {
   expect(matrix).toHaveLength(setSize)
   expect(vector).toHaveLength(setSize)
   expect(checkTwoDimMatrixSize(matrix)).toBe(true)
+})
+
+test('changeMatrixValue must return matrix', () => {
+  const size = 3
+  const val = 100
+  const x = 0
+  const y = 0
+  const a = [[1,2,3],[4,5,6],[7,8,9]]
+  const matrix = changeMatrixValue(x, y, a, val)
+
+  for (let i = 0; i < size; ++i) {
+    for (let j = 0; j < size; ++j) {
+      if (i === x && j === y) {
+        expect(matrix[i][j]).toBe(val)
+      } else {
+        expect(matrix[i][j]).toBe(a[i][j])
+      }
+    }
+  }
+})
+
+test('changeVectorValue must return Vector', () => {
+  const size = 3
+  const val = 100
+  const x = 0
+  const a = [1,2,3]
+  const vector = changeVectorValue(x, -1, a, val)
+
+  for (let i = 0; i < size; ++i) {
+    if (i === x) {
+      expect(vector[i]).toBe(val)
+    } else {
+      expect(vector[i]).toBe(a[i])
+    }
+  }
 })
