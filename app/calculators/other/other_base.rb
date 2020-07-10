a649223eedca6cfa0e96056d0807952df44bf72a
tarao1006 <taiga4562@gmail.com>
2020-07-11 02:10:22 +0900

    Modify backward and forward substitution

---
+++ app/calculators/other/other_base.rb
@@ -0,0 +1,39 @@
+
+class OtherBase
+
+  attr_reader :is_valid
+  def initialize(a, b)
+    @mat_a = Matrix[*a].map(&:to_f)
+    @mat_b = Vector[*b].map(&:to_f)
+    @n = @mat_a.column_size
+  end
+
+  def calculate(mat)
+  end
+
+  def core
+  end
+
+  def run
+    validate
+
+    unless @is_valid
+      return Vector[]
+    end
+
+    self.core
+
+    @mat_b
+  end
+
+  def is_square_matrix(mat)
+    mat.column_size == mat.row_size
+  end
+
+  def validate
+    @is_valid = true
+    if (!is_square_matrix(@mat_a)) or (@mat_a.row_size != @mat_b.size)
+      @is_valid = false
+    end
+  end
+end
--- app/controllers/api/v1/calculator_controller.rb
+++ app/controllers/api/v1/calculator_controller.rb
@@ -1,6 +1,6 @@
-require_relative '../../../calculators/lu_decomposition'
-require_relative '../../../calculators/forward_substitution'
-require_relative '../../../calculators/backward_substitution'
+require_relative '../../../calculators/other/lu_decomposition'
+require_relative '../../../calculators/other/forward_substitution'
+require_relative '../../../calculators/other/backward_substitution'
 
 module Api
   module V1
@@ -10,7 +10,8 @@ module Api
         mat = params[:matrix]
         b = params[:b]
 
-        ans = ForwardSubstitution.run(mat, b)
+        calculator = ForwardSubstitution.new(mat, b)
+        ans = calculator.run
 
         render json: { status: 'SUCCESS', ans: ans}, status: :ok
       end
@@ -19,22 +20,26 @@ module Api
         mat = params[:matrix]
         b = params[:b]
 
-        ans = BackwardSubstitution.run(mat, b)
+        calculator = BackwardSubstitution.new(mat, b)
+        ans = calculator.run
 
-        render json: { status: 'SUCCESS', ans: ans}, status: :ok end
+        p ans
+
+        render json: { status: 'SUCCESS', ans: ans}, status: :ok
+      end
 
       def lu_decomposition
-        calculator = LuDecomposition.new
-        size = params[:size]
-        mat = Array(params[:matrix])
-
-        unless calculator.validate(mat)
-          render json: { status: 'ERROR' }, status: :ok
-        end
-
-        matrix_l, matrix_u = calculator.calculate(mat)
-
-        render json: { status: 'SUCCESS', size: size, matrixL: matrix_l, matrixU: matrix_u }, status: :ok
+        # calculator = LuDecomposition.new
+        # size = params[:size]
+        # mat = Array(params[:matrix])
+        #
+        # unless calculator.validate(mat)
+        #   render json: { status: 'ERROR' }, status: :ok
+        # end
+        #
+        # matrix_l, matrix_u = calculator.calculate(mat)
+        #
+        # render json: { status: 'SUCCESS', size: size, matrixL: matrix_l, matrixU: matrix_u }, status: :ok
       end
     end
   end
