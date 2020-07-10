dc9ff53ec86db2886c2478d3bac72473042f5c8f
tarao1006 <taiga4562@gmail.com>
2020-07-10 13:11:48 +0900

    Add title and initroduce flex box to body

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -1,54 +1,58 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import Link from './components/linkcomposition'
-import Title, { siteTitle } from './components/title'
+import { siteTitle } from './components/title'
 
 const Home = () => {
-  console.log(window.innerHeight)
 
   useEffect(() => {
     document.title = `ホーム | ${siteTitle}`
-    console.log(window.innerHeight)
   })
 
   return (
-    <Wrapper　height={ `${window.innerHeight}px` }>
-      <Link to="/linear_equation">
-        <Category
-          name="連立一次方程式"
-          description="連立一次方程式を解きます。"
-        />
-      </Link>
-      <Link to="/linear_equation">
-        <Category
-          name="固有値"
-          description="行列の固有値を求めます。"
-        />
-      </Link>
-      <Link to="/linear_equation">
-        <Category
-          name="その他"
-          description="その他有名なものです。"
-        />
-      </Link>
-    </Wrapper>
+    <>
+      <Title>
+        数値計算ライブラリ
+      </Title>
+      <Sub>
+      「工学のための数値計算」に載っている基本的な数値計算を実行できます。
+      </Sub>
+      <Wrapper>
+        <Link to="/linear_equation">
+          <Category
+            name="連立一次方程式"
+            description="連立一次方程式を解きます。"
+          />
+        </Link>
+        <Link to="/linear_equation">
+          <Category
+            name="固有値"
+            description="行列の固有値を求めます。"
+          />
+        </Link>
+        <Link to="/linear_equation">
+          <Category
+            name="その他"
+            description="その他有名なものです。"
+          />
+        </Link>
+      </Wrapper>
+    </>
   )
 }
 
 export default Home
 
-const Category = ({ name, description, children }) => {
+const Category = ({ name, description }) => {
 
   return (
-    <Body height={ window.innerHeight - 30 }>
-      <Method>
+    <Body height={ window.innerHeight - 300 }>
       <Name>
         { name }
       </Name>
       <Description>
         { description }
       </Description>
-      </Method>
     </Body>
   )
 }
@@ -58,6 +62,17 @@ const Wrapper = styled.div`
   grid-template-columns: repeat(3, minmax(100px, 1fr));
   gap: 15px;
   min-width: 700px;
+`
+
+const Title = styled.h1`
+  font-size: 3em;
+  text-align: center;
+`
+
+const Sub = styled.h2`
+  font-size: 1.2em;
+  color: rgb(4, 4, 4);
+  text-align: center;
 `
 
 const Body = styled.div`
@@ -70,15 +85,15 @@ const Body = styled.div`
   transition: all 0.4s ease 0s;
   color: black;
   height: ${({ height }) => `${height}px`};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
   &:hover {
     background: black;
     color: white;
   }
-`
-
-const Method = styled.div`
-  padding: 20em 0;
 `
 
 const Name = styled.div`
