5243ada5cf9c8dbb960a8cb12bfd736bc48b02c1
tarao1006 <taiga4562@gmail.com>
2020-07-11 13:03:42 +0900

    Add lu decomposition skeleton

--- frontend/src/components/calculator/other/lu_decomposition.jsx
+++ frontend/src/components/calculator/other/lu_decomposition.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import DecompositionContainer from '../../decompositioncontainer'
+import useForwardSubstitution from '../../../actions/other/forward_substitution'
+import Matrix from '../../matrix/matrix'
 
 const LuDecomposition = () => {
+  const title = "LU分解"
+  const [ status, setStatus ] = useState(false)
+  const [ executed, setExecuted ] = useState(false)
+  const [ loading, setLoading ] = useState(false)
+  const [ matL, setMatL ] = useState([])
+  const [ matU, setMatU ] = useState([])
+  const { size, coefficientMatrix } = useForwardSubstitution()
+
+  const execute = async () => {
+    setExecuted(false)
+    setStatus(false)
+    setLoading(true)
+    try {
+      const result = await axios.post(
+        'http://localhost:3001/api/v1/calculator/lu_decomposition', {
+          "size": size,
+          "matrix": coefficientMatrix
+        })
+
+        setStatus(result.data.status === 'SUCCESS')
+        setMatL(result.data.matrixL)
+        setMatU(result.data.matrixU)
+        setExecuted(true)
+      } catch (error) {
+        setStatus(false)
+    }
+    setLoading(false)
+  }
 
   return (
-    <>LU分解</>
+    <>
+      <DecompositionContainer
+        title={ title }
+        execute={ execute }
+        status={ status }
+        executed={ executed }
+        loading={ loading }
+        result={
+          executed &&
+          {
+            mat_l: <Matrix
+              rowCount={ matL.length }
+              columnCount={ matL.length }
+              values={ matL }
+              readOnly={ true }
+            />,
+            mat_u: <Matrix
+              rowCount={ matU.length }
+              columnCount={ matU.length }
+              values={ matU }
+              readOnly={ true }
+            />
+          }
+        }
+        setStatus={ setStatus }
+        setExecuted={ setExecuted }
+        useLinearEquation={ useForwardSubstitution }
+      />
+    </>
   )
 }
 
---
+++ frontend/src/components/decompositioncontainer.jsx
@@ -0,0 +1,144 @@
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import Matrix from './matrix/matrix'
+import MatrixSize from './matrixsize'
+import ExecuteButton from './executebutton'
+import { siteTitle } from './title'
+import { updateValues } from '../lib/linearequation'
+import { Label } from './paraminput'
+
+const DecompositionContainer = ({ children, title, execute, result, status, loading, executed, setStatus, setExecuted, useLinearEquation }) => {
+
+  useEffect(() => {
+    document.title = `${title} | ${siteTitle}`
+  })
+
+  const { size, coefficientMatrix, update, id } = useLinearEquation()
+
+  const handleUpdateClick = () => {
+    const [s, mat, vec] = updateValues(size)
+    update(s, mat, vec)
+    setStatus(false)
+    setExecuted(false)
+  }
+
+  return (
+    <>
+      <Title>
+        { title }
+      </Title>
+
+      <HeadContainer>
+        <MatrixSize setStatus={ setStatus } setExecuted={ setExecuted } />
+        <GenerateButton onClick={ handleUpdateClick } >
+          生成
+        </GenerateButton>
+      </HeadContainer>
+
+      { children }
+
+      <Container>
+        <MatrixWrapper>
+          <Label>行列</Label>
+          <Matrix
+            rowCount={ size }
+            columnCount={ size }
+            values={ coefficientMatrix }
+            id={ id }
+          />
+        </MatrixWrapper>
+
+        <Wrapper>
+          <ExecuteButton execute={ execute } />
+        </Wrapper>
+
+        { loading &&
+          (<Wrapper fontSize="2">
+            計算中...
+          </Wrapper>)}
+        { executed &&
+          (<StatusWrapper status={ status }>
+            { status ? "Succeeded" : "Failed" }
+          </StatusWrapper>)}
+        { status &&
+          (<>
+            <MatrixWrapper>
+            <Label>L行列</Label>
+              { result.mat_l }
+            </MatrixWrapper>
+            <MatrixWrapper>
+            <Label>U行列</Label>
+              { result.mat_u }
+            </MatrixWrapper>
+          </>)}
+
+      </Container>
+    </>
+  )
+}
+
+export default DecompositionContainer
+
+const Title = styled.h1`
+  font-size: 2em;
+  text-align: center;
+`
+
+const Container = styled.div`
+  max-width: 700px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+const HeadContainer = styled.div`
+  display: flex;
+`
+
+const GenerateButton = styled.button`
+  width: 100px;
+  color: black;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.4em 1em;
+  border: none;
+  border-radius: 3px;
+  box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.1), 0px 2px 10px 0px rgba(0, 0, 0, 0.08);
+  transition: all 0.25s ease 0s;
+  background: white;
+  cursor: pointer;
+  outline: none;
+  appearance: none;
+
+   &:hover {
+    background: lightgray;
+  }
+`
+
+const MatrixWrapper = styled.div`
+  max-width: 700px;
+  overflow: scroll;
+`
+
+const VectorWrapper = styled.div`
+  max-width: 700px;
+`
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  font-size: ${props => `${props.fontSize}em`}
+`
+
+const StatusWrapper = styled.div`
+  margin: 0 auto;
+  padding: 10px 100px;
+  border-radius: 10px;
+  color: white;
+  background: ${props => props.status ? "#5cb85c" : "#d9534f"};
+`
+
+const IterWrapper = styled.div`
+  margin: 0 auto;
+  font-size: 1.5em;
+`
