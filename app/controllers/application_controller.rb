61dd5ba71e5b396030cd7d6ac1a4d62803034215
tarao1006 <taiga4562@gmail.com>
2020-07-13 00:36:32 +0900

    Fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
   def fallback_index_html
     respond_to do |format|
       format.html {
-        render body: Rails.root.join('pubic/index.html').read
+        render body: Rails.root.join('public/index.html').read
       }
     end
   end
