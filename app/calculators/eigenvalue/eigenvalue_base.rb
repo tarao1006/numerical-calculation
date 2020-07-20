4a70ce80c36190327a4af5dd21d183b64dcbc64f
tarao1006 <taiga4562@gmail.com>
2020-07-21 01:51:12 +0900

    Add power method

--- app/calculators/eigenvalue/eigenvalue_base.rb
+++ app/calculators/eigenvalue/eigenvalue_base.rb
@@ -1,1 +1,27 @@
+require 'matrix'
 
+module Eigenvalue
+  class EigenvalueBase
+
+    def initialize(a, eps = 1e-6, n = 1000)
+      @mat_a = Matrix[*a].map(&:to_f)
+      @eps = eps
+      @n = n.to_i
+    end
+
+    def core
+    end
+
+    def run
+      self.core
+
+      @status = 'SUCCESS'
+      if @iter > @n
+        @status = 'FAILURE'
+        @eigenvalue = 0
+      end
+
+      [@eigenvalue, @status]
+    end
+  end
+end
--- app/controllers/api/v1/eigenvalue_controller.rb
+++ app/controllers/api/v1/eigenvalue_controller.rb
@@ -1,9 +1,18 @@
+require_relative '../../../calculators/eigenvalue/power_method'
+
 module Api
   module V1
     class EigenvalueController < ApplicationController
 
-      
+      def power_method
+        mat = params[:matrix]
 
+        calculator = Eigenvalue::PowerMethod.new mat
+        ans, status = calculator.run
+
+        p ans
+        render json: { status: status, ans: ans }
+      end
     end
   end
 end
